30cc0648aae0300fad014b245f561b4c49de6579
Fathandi <168356153+Fathandi@users.noreply.github.com>
2025-01-02 17:28:55 +0700

    Update advanced.js

--- advanced.js
+++ advanced.js
@@ -1,193 +1,193 @@
-// // ========================================
-// // =                                      =
-// // =                Part 1                =
-// // =                                      =
-// // ========================================
+// ========================================
+// =                                      =
+// =                Part 1                =
+// =                                      =
+// ========================================
 
 
-// // ========================================
-// // 1. Object Literal
-// // ========================================
+// ========================================
+// 1. Object Literal
+// ========================================
 
-// let mahasiswa1 = {
-//     nama: 'Rizky',
-//     energy: 10,
-//     makan: function(porsi){ 
-//         this.energy += porsi;
-//         console.log(`selamat datang  ${this.nama}, selamat makan!`);
-//     }
-// }
+let mahasiswa1 = {
+    nama: 'Rizky',
+    energy: 10,
+    makan: function(porsi){ 
+        this.energy += porsi;
+        console.log(`selamat datang  ${this.nama}, selamat makan!`);
+    }
+}
 
-// let mahasiswa2 = {
-//     nama: 'Japril',
-//     energy: 10,
-//     makan: function(porsi){
-//         this.energy += porsi;
-//         console.log(`selamat datang  ${this.nama}, selamat makan!`);
-//     }
-// }
-
-
-// // ========================================
-// // 2. Function Declaration
-// // Object.create()
-// // ========================================
-
-// const methodMahasiswa = {
-//     makan : function(porsi){
-//         this.energy += porsi;
-//         console.log(`halo ${this.nama}, selamat makan`)
-//     },
-
-//     main : function(jam){
-//         this.energy -= jam;
-//         console.log(`halo ${this.nama}, selamat bermain`)
-//     },
-
-//     tidur : function(jam){
-//         this.energy += jam * 2;
-//         console.log(`halo ${this.nama}, selamat tidur`)
-//     }
-// };
-// function Mahasiswa(nama, energy){
-//     let mahasiswa = Object.create(methodMahasiswa);
-//     mahasiswa.nama = nama;
-//     mahasiswa.energy = energy;
-
-//     return mahasiswa;
-// }
-
-// let Rizky = Mahasiswa('Rizky', 10);
-// let Japril = Mahasiswa('Japril', 20);
+let mahasiswa2 = {
+    nama: 'Japril',
+    energy: 10,
+    makan: function(porsi){
+        this.energy += porsi;
+        console.log(`selamat datang  ${this.nama}, selamat makan!`);
+    }
+}
 
 
-// // ========================================
-// // 3. Constructor Function
-// // Keyword New
-// // ========================================
+// ========================================
+// 2. Function Declaration
+// Object.create()
+// ========================================
 
-// function Mahasiswa(nama, energy){
-//     this.nama = nama;
-//     this.energy = energy;
+const methodMahasiswa = {
+    makan : function(porsi){
+        this.energy += porsi;
+        console.log(`halo ${this.nama}, selamat makan`)
+    },
 
-//     this.makan = function(porsi){
-//         this.energy += porsi;
-//         console.log(`halo ${this.nama}, selamat makan`)
-//     }
+    main : function(jam){
+        this.energy -= jam;
+        console.log(`halo ${this.nama}, selamat bermain`)
+    },
 
-//     this.main = function(jam){
-//         this.energy -= jam;
-//         console.log(`halo ${this.nama}, selamat bermain`)
-//     }
-// }
+    tidur : function(jam){
+        this.energy += jam * 2;
+        console.log(`halo ${this.nama}, selamat tidur`)
+    }
+};
+function Mahasiswa(nama, energy){
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energy = energy;
 
-// let Rizky = new Mahasiswa('Rizky', 10);
-// let Japril = new Mahasiswa('Japril', 20);
+    return mahasiswa;
+}
+
+let Rizky = Mahasiswa('Rizky', 10);
+let Japril = Mahasiswa('Japril', 20);
 
 
+// ========================================
+// 3. Constructor Function
+// Keyword New
+// ========================================
 
-// // ========================================
-// // 4. Prototype
-// // ========================================
+function Mahasiswa(nama, energy){
+    this.nama = nama;
+    this.energy = energy;
 
-// function Mahasiswa(nama, energy){
-//     this.nama = nama;
-//     this.energy = energy;
-// }
+    this.makan = function(porsi){
+        this.energy += porsi;
+        console.log(`halo ${this.nama}, selamat makan`)
+    }
 
-// Mahasiswa.prototype.makan = function(porsi){
-//     this.energy += porsi;
-//     return `halo ${this.nama}, selamat makan`;
-// }
+    this.main = function(jam){
+        this.energy -= jam;
+        console.log(`halo ${this.nama}, selamat bermain`)
+    }
+}
 
-// Mahasiswa.prototype.main = function(jam){
-//     this.energy -= jam;
-//     return `halo ${this.nama}, selamat bermain`;
-// }
-
-// Mahasiswa.prototype.tidur = function(jam){
-//     this.energy += jam * 2;
-//     return `halo ${this.nama}, selamat tidur`;
-// }
-
-// let Rizky = new Mahasiswa('Rizky', 10);
-// let Japril = new Mahasiswa('Japril', 20);
+let Rizky = new Mahasiswa('Rizky', 10);
+let Japril = new Mahasiswa('Japril', 20);
 
 
 
-// // ========================================
-// // 5. Prototype Versi Class
-// // ========================================
+// ========================================
+// 4. Prototype
+// ========================================
 
-// class Mahasiswa {
-//     constructor(nama, energy) {
-//         this.nama = nama;
-//         this.energy = energy;
-//         }
+function Mahasiswa(nama, energy){
+    this.nama = nama;
+    this.energy = energy;
+}
 
-//     makan(porsi) {
-//         this.energy += porsi;
-//         return `halo ${this.nama}, selamat makan`;
-//         }
+Mahasiswa.prototype.makan = function(porsi){
+    this.energy += porsi;
+    return `halo ${this.nama}, selamat makan`;
+}
 
-//     main(jam) {
-//         this.energy -= jam;
-//         return `halo ${this.nama}, selamat bermain`;
-//         }
+Mahasiswa.prototype.main = function(jam){
+    this.energy -= jam;
+    return `halo ${this.nama}, selamat bermain`;
+}
 
-//     tidur(jam) {
-//         this.energy += jam * 2;
-//         return `halo ${this.nama}, selamat tidur`;
-//         }
-//     }
+Mahasiswa.prototype.tidur = function(jam){
+    this.energy += jam * 2;
+    return `halo ${this.nama}, selamat tidur`;
+}
 
-// let Rizky = new Mahasiswa('Rizky', 10);
-// let Japril = new Mahasiswa('Japril', 20);
-// console.log(Rizky.makan(5));
-// console.log(Japril.main(3));
-// console.log(Rizky.tidur(4));
+let Rizky = new Mahasiswa('Rizky', 10);
+let Japril = new Mahasiswa('Japril', 20);
 
 
 
-// // ========================================
-// // =                                      =
-// // =                Part 2                =
-// // =                                      =
-// // ========================================
+// ========================================
+// 5. Prototype Versi Class
+// ========================================
+
+class Mahasiswa {
+    constructor(nama, energy) {
+        this.nama = nama;
+        this.energy = energy;
+        }
+
+    makan(porsi) {
+        this.energy += porsi;
+        return `halo ${this.nama}, selamat makan`;
+        }
+
+    main(jam) {
+        this.energy -= jam;
+        return `halo ${this.nama}, selamat bermain`;
+        }
+
+    tidur(jam) {
+        this.energy += jam * 2;
+        return `halo ${this.nama}, selamat tidur`;
+        }
+    }
+
+let Rizky = new Mahasiswa('Rizky', 10);
+let Japril = new Mahasiswa('Japril', 20);
+console.log(Rizky.makan(5));
+console.log(Japril.main(3));
+console.log(Rizky.tidur(4));
 
 
-// // ========================================
-// // 1. Execution Context, Hoisting, Scope
-// // ========================================
 
-// var nama = 'Rizky';
-// console.log(nama);
-
-// // creation phase pada global context
-// // nama var = undefined
-// // nama function = fn()
-// // hoisting
-// // window = global object
-// // this = window
+// ========================================
+// =                                      =
+// =                Part 2                =
+// =                                      =
+// ========================================
 
 
-// // creation phase
+// ========================================
+// 1. Execution Context, Hoisting, Scope
+// ========================================
 
-// console.log(sayHello());
+var nama = 'Rizky';
+console.log(nama);
 
-// var nama = 'Rizky';
-// var umur = 20;
+// creation phase pada global context
+// nama var = undefined
+// nama function = fn()
+// hoisting
+// window = global object
+// this = window
 
-// function sayHello(){
-//     return `halo ${nama} berumur ${umur} tahun`;
-// }
+
+// creation phase
+
+console.log(sayHello());
+
+var nama = 'Rizky';
+var umur = 20;
+
+function sayHello(){
+    return `halo ${nama} berumur ${umur} tahun`;
+}
 
 
-// // function membuat execution context
-// // yang di dalamnya terdapat creation phase dan execution phase
-// // window
-// // argumen
-// // hoisting
+// function membuat execution context
+// yang di dalamnya terdapat creation phase dan execution phase
+// window
+// argumen
+// hoisting
 
 
 var nama = 'Rizky Ramadhan';
@@ -201,12 +201,12 @@ function cetakURL(username){
 console.log(cetakURL(japril));
 
 
-// function a(){
-//     console.log('ini adalah function a');
-//     function b(){
-//         console.log('ini adalah function b');
-//         function c(){
-//             console.log('ini adalah function c');
-//         } c();
-//     } b();
-// } a();
+function a(){
+    console.log('ini adalah function a');
+    function b(){
+        console.log('ini adalah function b');
+        function c(){
+            console.log('ini adalah function c');
+        } c();
+    } b();
+} a();
